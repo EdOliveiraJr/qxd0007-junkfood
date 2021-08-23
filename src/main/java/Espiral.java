9fb88f3ec81fe0bd03ea2f0438eb65d1c42164f8
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2021-08-22 21:56:23 -0300

    Atualização no código

--- src/main/java/Espiral.java
+++ src/main/java/Espiral.java
@@ -1,2 +1,36 @@
 public class Espiral{
+    private String nomeProduto = " - ";
+    private int qtdProduto;
+    private double preco;
+
+    public Espiral(){
+    }
+
+    public String getNomeDoProduto(){
+        return nomeProduto;
+    }
+
+    public int getQuantidade() {
+        return qtdProduto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setNomeDoProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public void setQuantidade(int qtdProduto) {
+        this.qtdProduto = qtdProduto;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void darSaida(){
+        qtdProduto--;
+    }
 }
--- src/main/java/Maquina.java
+++ src/main/java/Maquina.java
@@ -1,46 +1,108 @@
-public class Maquina {
+import java.util.ArrayList;
+import java.util.Vector;
 
+import jdk.nashorn.internal.runtime.regexp.joni.ast.QuantifierNode;
+
+public class Maquina {
+    private int qtdEspirais;
+    private int maximoProdutos;
+    private double faturamento;
+    private double saldo;
+    
+    ArrayList <Espiral> espirais;
 
     public Maquina(int qtdEspirais, int maximoProdutos){
+        this.qtdEspirais = qtdEspirais;
+        this.maximoProdutos = maximoProdutos;
+        espirais = new ArrayList<>();
+        
+        for (int i = 0; i < qtdEspirais; i++) {
+            espirais.add(new Espiral());
+        }
     }
 
     public double getFaturamento() {
-        return 0;
+        return faturamento;
     }
 
     public int getMaximoProdutos() {
-        return 0;
+        return maximoProdutos;
     }
 
     public double getSaldoCliente() {
-        return 0;
+        return saldo;
     }
 
     public int getSizeEspirais(){
-        return 0;
+        return qtdEspirais;
     }
 
     public Espiral getEspiral(int indice){
-        return null;
+        if(indice >= getSizeEspirais() || indice < 0){
+            return null;
+        }
+        return espirais.get(indice);
+    }
+
+    public int getProdutosNasEspirais(){
+        int total = 0;
+        for (Espiral espiral : espirais) {
+            total += espiral.getQuantidade();
+        }
+        return total;
     }
 
     public boolean inserirDinheiro(double value){
-        return false;
-    }
-
-    public double receberTroco(){
-        return 0;
-    }
-
-    public boolean alterarEspiral(int indice, String nome, int quantidade, double preco){
+        if(value <= 0){
+            return false;
+        }
+        this.saldo += value;
         return true;
     }
 
+    public double receberTroco(){
+        double troco = saldo;
+        saldo = 0.0;
+        return troco;
+    }
+
+    public boolean alterarEspiral(int indice, String nome, int quantidade, double preco){
+        if(indice >= getSizeEspirais() || indice < 0){
+            return false;
+        }
+        if(maximoProdutos >= getProdutosNasEspirais() + quantidade){
+            Espiral nova = new Espiral();
+            nova.setNomeDoProduto(nome);
+            nova.setQuantidade(quantidade);
+            nova.setPreco(preco);
+            espirais.set(indice, nova);
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
     public boolean limparEspiral(int indice){
+        
+        if(indice >= getSizeEspirais() || indice < 0){
+            return false;
+        }
+
+        espirais.set(indice, new Espiral());
         return true;
     }
 
     public boolean vender(int indice){
-        return false;
+        if(indice < 0 || indice >= getSizeEspirais()){
+            return false;
+        }
+        if(saldo < getEspiral(indice).getPreco()){
+            return false;
+        }
+        saldo -= espirais.get(indice).getPreco();
+        getEspiral(indice).darSaida();       
+        return true;
+        
     }
 }
